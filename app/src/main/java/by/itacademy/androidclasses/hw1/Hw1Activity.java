5a0e4c0b51290ed28572190bd453c7a8ab8b7210
KsyushaDrebenchuk <drebenchuk.ksysha@gmail.com>
2019-02-25 14:20:42 +0300

    Made the layout of the activity.

--- app/src/main/java/by/itacademy/androidclasses/hw1/Hw1Activity.java
+++ app/src/main/java/by/itacademy/androidclasses/hw1/Hw1Activity.java
@@ -1,14 +1,15 @@
-package by.itacademy.androidclasses;
+package by.itacademy.androidclasses.hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import by.itacademy.androidclasses.R;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Hw1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hw1);
     }
 }
