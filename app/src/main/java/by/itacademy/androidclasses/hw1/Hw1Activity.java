63fd339c51cceacfb28bfe65c6d450b3fba74a33
KsyushaDrebenchuk <drebenchuk.ksysha@gmail.com>
2019-02-25 22:40:23 +0300

    Home work 1 complete.

--- app/src/main/java/by/itacademy/androidclasses/hw1/Hw1Activity.java
+++ app/src/main/java/by/itacademy/androidclasses/hw1/Hw1Activity.java
@@ -1,6 +1,5 @@
 package by.itacademy.androidclasses.hw1;
 
-import androidx.appcompat.app.AppCompatActivity;
 import by.itacademy.androidclasses.R;
 
 import android.app.Activity;
@@ -15,16 +14,32 @@ public class Hw1Activity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw1);
+
+        findViewById(R.id.textView1).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                changeText();
+                changeColor();
+            }
+        });
+
+        findViewById(R.id.textView2).setOnClickListener(textView2ClickListener);
     }
 
-    public void changeText() {
-        TextView textView1 = findViewById(R.id.textView1);
-        TextView textView2 = findViewById(R.id.textView2);
-        CharSequence text1 = textView1.getText();
-        CharSequence text2 = textView2.getText();
-        textView2.setText(text1);
-        textView1.setText(text2);
+    @Override
+    public void onClick(View v) {
+        changeText();
+        changeColor();
     }
+
+    private View.OnClickListener textView2ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            changeColor();
+            changeText();
+        }
+    };
 
     public void changeColor() {
         TextView textView1 = findViewById(R.id.textView1);
@@ -37,9 +52,12 @@ public class Hw1Activity extends Activity implements View.OnClickListener {
         textView2.setBackgroundColor(colorCode1);
     }
 
-    @Override
-    public void onClick(View v) {
-        changeText();
-        changeColor();
+    public void changeText() {
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        CharSequence text1 = textView1.getText();
+        CharSequence text2 = textView2.getText();
+        textView2.setText(text1);
+        textView1.setText(text2);
     }
 }
